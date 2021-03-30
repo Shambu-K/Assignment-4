b94e4b2db9500c730c1894d00fa3269849208e5c
Shambu-K <80613838+Shambu-K@users.noreply.github.com>
2021-03-30 18:42:55 +0530

    Add files via upload

---
+++ Assignment-4.py
@@ -0,0 +1,21 @@
+# program to plot PDF
+import numpy as np 
+import matplotlib.pyplot as plot
+
+#defining the PDF
+def F(x):
+  if(x>1) and (x<6):
+    return 1/5;
+  else:
+    return 0  
+
+    #plotting Pdf 
+X = np.linspace(-8,8,100000)
+
+Y = [F(x) for x in X]
+plot.xlabel('y')
+plot.ylabel('$F(y)$')
+plot.plot(X,Y)
+plot.grid()
+plot.savefig('Assignment4.png' , dpi=125)
+plot.show()    
