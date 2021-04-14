e41e37fc9efc65d78a5490338a9389aef53ddada
Shambu-K <80613838+Shambu-K@users.noreply.github.com>
2021-04-14 18:19:07 +0530

    Add files via upload

---
+++ Assignment-4.py
@@ -0,0 +1,23 @@
+# program to plot PDF
+import numpy as np 
+import matplotlib.pyplot as plot
+
+#defining the Cdf
+def F(x):
+  if(x>1) and (x<6):
+    return (x-1)/5;
+  if(x>=6):
+    return 1;  
+  else:
+    return 0  
+
+    #plotting Cdf 
+X = np.linspace(-8,8,100000)
+
+Y = [F(x) for x in X]
+plot.xlabel('y')
+plot.ylabel('$F(y)$')
+plot.plot(X,Y)
+plot.grid()
+plot.savefig('Assignment4.png' , dpi=125)
+plot.show()    
